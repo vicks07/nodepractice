2bd177e230c1eac11b128cf9c24b903aa9ff2b4d
Vikram T <vikram.january@gmail.com>
2018-10-11 16:36:25 +0530

    Add files via upload

--- playground/json.js
+++ playground/json.js
@@ -9,3 +9,17 @@
 // var personString = '{"name":"Vikram","age":25}';
 // var person = JSON.parse(personString);
 // console.log(personString,person,typeof person,person.name,person.age);
+
+const fs = require('fs');
+var originalNote = {
+    title:'Some title',
+    body:'Some body'
+};
+//originalNoteString
+originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json',originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof noteString);
+console.log(note);
