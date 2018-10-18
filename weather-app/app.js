2a54903af02a250973a020c24c39c9e1e3c2da8e
vicks07 <vikram.january@gmail.com>
2018-10-18 20:47:05 +0530

    Google map response using http request

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,7 @@ request({
     url:'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDm_uDnlHgdjHFz3fF3X8t06D1JOEw5568&address=mit%20manipal',
     json:true
 },(error,response,body)=>{
-    console.log(error,body,response);
+    //console.log(JSON.stringify(body,undefined,2));
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
 });
